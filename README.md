3c17a3797cd44087002ce8d6bd4fa6a50dfbf45f
Azunyan <s15a1302@bene.fit.ac.jp>
2017-05-29 12:34:00 +0900

    mongodb connection test

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # RsaChatServer
+
+1. install mongoDB
+
+2. pip install pymongo
+
--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+import pymongo
 
+# connection to mongoDB
+connect = pymongo.MongoClient('localhost', 27017)
 
-print "test"
+# create data base
+db = connect['TestDataBase']
+
+# create collection
+co = db['TestCollection']
+
+# save one test data
+co.insert_one({"test": 1, "test2": 0})
+
+# print all collection data
+for collection in co.find():
+    print collection
